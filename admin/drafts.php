3104243662eacac420299ba4f1997eee688e2638
buretj <buretj@buretj-N3000.(none)>
2014-09-09 15:22:33 +0200

    bug z-index resolved

--- admin/drafts.php
+++ admin/drafts.php
@@ -8,11 +8,15 @@ if (!$log->is_logued()) {
 	header('Location: login.php?case=disconnect');
 }else{
 	$bdd = new tapdo();
+	$rights = rights($bdd);
 	if (isset($_POST['get']) && $_POST['get'] ="drafts") {
 		$drafts = $bdd->get_all('draft');
 		print_r(json_encode($drafts));
 	}elseif(isset($_POST['del'])){
-		$bdd->suppr('draft', 'id', $_POST['del']);
+		if ($rights > 2)
+			header('Location: index.php?no=no');
+		else
+			$bdd->suppr('draft', 'id', $_POST['del']);
 	}elseif (!isset($_POST['name']) || !isset($_POST['txt'])) {
 		header('Location: index.php?wrong=params drafts ajax');
 	}else{
